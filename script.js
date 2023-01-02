676e5f67b449188a990115dff3ff7255ded1daac
Melanie Akas <pmaccount@cindyy-mbp.home>
2023-01-02 13:13:34 +0000

    added linkedin and dps

---
+++ script.js
@@ -0,0 +1,6 @@
+{/* <a href="javascript: document.body.scrollIntoView(false);">Scroll to bottom</a> */}
+
+
+document.getElementById("scroll-to-bottom").addEventListener("click", function () {
+    document.body.scrollIntoView(false);
+  });
